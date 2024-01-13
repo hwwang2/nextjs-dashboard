70f3a756bc48eaa7cf17f7474fa8b3faa26fa1eb
wanghongwei <hwwang112244@gmail.com>
2024-01-13 09:45:24 +0800

    finished chapter 13

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -167,8 +167,7 @@ export async function fetchInvoiceById(id: string) {
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
-    // throw new Error('Failed to fetch invoice.');
-    return NaN;
+    throw new Error('Failed to fetch invoice.');
   }
 }
 
